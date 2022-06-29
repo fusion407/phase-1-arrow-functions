77b3bcaf85fecebe1ab6ed43a2af9b6e2fac7399
fusion407 <fusion407@gmail.com>
2022-06-28 22:26:58 -0500

    commit tree 74e400da48ff54c5b60f0c68491ea6039835f577

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+const square = (param) => {
+    return param * param;
+};
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
